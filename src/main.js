880e1e6dd3991a5c71b38603ec2c0c3a5372403f
Vincent <vincent@smartmile.eu>
2021-07-13 15:38:38 +0300

    use Listr to list all tasks with a loader

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
@@ -11,6 +14,15 @@ async function copyTemplateFiles(options) {
   return copy(options.templateDirectory, options.targetDirectory, {
     clobber: false
   });
+}
+
+async function initGit(options) {
+  const result = await execa('git', ['init'], {
+    cwd: options.targetDirectory
+  });
+  if (result.failed) {
+    return Promise.reject(new Error('Failed to initialize git'));
+  }
 }
 
 export async function createProject(options) {
@@ -34,8 +46,30 @@ export async function createProject(options) {
     process.exit(1);
   }
 
-  console.log('Copy project files');
-  await copyTemplateFiles(options);
+  const tasks = new Listr([
+    {
+      title: 'Copy project files',
+      task: () => copyTemplateFiles(options)
+    },
+    {
+      title: 'Initialize git',
+      task: () => initGit(options),
+      enabled: () => options.git
+    },
+    {
+      title: 'Install dependencies',
+      task: () =>
+        projectInstall({
+          cwd: options.targetDirectory
+        }),
+      skip: () =>
+        !options.runInstall
+          ? 'Pass --install to automatically install dependencies'
+          : undefined
+    }
+  ]);
+
+  await tasks.run();
 
   console.log('%s Project ready', chalk.green.bold('DONE'));
   return true;
